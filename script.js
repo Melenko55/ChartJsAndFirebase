3d81ae6ec64d139b0ebe481ed5171adac814ce9f
Melya <melenko55@gmail.com>
2021-12-04 23:53:21 +0200

    defaultopions check

--- script.js
+++ script.js
@@ -1,59 +1,53 @@
 const CHART = document.getElementById('lineChart')
 
+const defaultData = {
+            backgroundColor: 'rgba(2, 255, 0, 0.4)',
+            borderCapStyle: 'square',
+            borderColor: 'rgba(0, 0, 0, 0.1)',
+            borderDash: [],
+            borderDashOffset: 0,
+            borderJoinStyle: 'bevel',
+            borderWidth: 20,
+            clip:  {left: 0, top: 0, right: 0, bottom: 0},
+            cubicInterpolationMode: 'default',
+            data: [65, 59, 80, 81, 56, 55, 40],
+            fill: false,
+            hoverBackgroundColor: 'rgba(244, 0, 0, 1)',
+            hoverBorderCapStyle: 'square',
+            hoverBorderColor: 'red',
+            hoverBorderDash: [100],
+            hoverBorderDashOffset: 2,
+            hoverBorderJoinStyle: 'bevel',
+            hoverBorderWidth: 106,
+            label: 'Default Label',
+            indexAxis: 'x',
+            order: 0,
+            pointBackgroundColor: 'rgba(0, 0, 0, 1)',
+            pointBorderColor: 'rgba(255, 0, 0, 0.6)',
+            pointBorderWidth: 10,
+            pointHitRadius: 15,
+            pointHoverBackgroundColor: 'rgba(0, 255, 0, 1)',
+            pointHoverBorderColor: undefined,
+            pointHoverBorderWidth: 1,
+            pointHoverRadius: 4,
+            pointRadius: 5,
+            pointRotation: 90,
+            pointStyle: 'circle',
+            segment: undefined,
+            showLine: true,
+            spanGaps: undefined,
+            stack: 'line',
+            tension: 0,
+            xAxisId: 'first x axis',
+            yAxisId: 'first y axis'
+        }
+
 let lineChart = new Chart(CHART, {
     type: 'line',
     data: {
         labels: ['May', 'June', 'July', "August", 'September', 'October', 'November'],
-        datasets: [{
-            axis: 'y',
-            label: 'My First Dataset',
-            data: [65, 59, 80, 81, 56, 55, 40],
-            fill: false,
-            backgroundColor: [
-            'rgba(255, 99, 132, 0.2)',
-            'rgba(255, 159, 64, 0.2)',
-            'rgba(255, 205, 86, 0.2)',
-            'rgba(75, 192, 192, 0.2)',
-            'rgba(54, 162, 235, 0.2)',
-            'rgba(153, 102, 255, 0.2)',
-            'rgba(201, 203, 207, 0.2)'
-            ],
-            borderColor: [
-            'rgb(255, 99, 132)',
-            'rgb(255, 159, 64)',
-            'rgb(255, 205, 86)',
-            'rgb(75, 192, 192)',
-            'rgb(54, 162, 235)',
-            'rgb(153, 102, 255)',
-            'rgb(201, 203, 207)'
-            ],
-            borderWidth: 1
-        },
-    {
-            axis: 'y',
-            label: 'My Second Dataset',
-            data: [120, 52, 34, 12, 22, 56, 42],
-            fill: true,
-            lineTension: 0,
-            backgroundColor: [
-            'rgba(255, 99, 132, 0.2)',
-            'rgba(255, 159, 64, 0.2)',
-            'rgba(255, 205, 86, 0.2)',
-            'rgba(75, 192, 192, 0.2)',
-            'rgba(54, 162, 235, 0.2)',
-            'rgba(153, 102, 255, 0.2)',
-            'rgba(201, 203, 207, 0.2)'
-            ],
-            borderColor: [
-            'rgb(255, 99, 132)',
-            'rgb(255, 159, 64)',
-            'rgb(255, 205, 86)',
-            'rgb(75, 192, 192)',
-            'rgb(54, 162, 235)',
-            'rgb(153, 102, 255)',
-            'rgb(201, 203, 207)'
-            ],
-            borderWidth: 1
-        }]
+        datasets: [
+            defaultData
+        ]
         }
 })
